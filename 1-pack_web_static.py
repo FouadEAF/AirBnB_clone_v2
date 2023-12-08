f28e6fd883a2d5d8d84367c15aae7a9bafda15f4
Developper_EAF <50346738+FouadEAF@users.noreply.github.com>
2023-12-08 20:47:46 +0100

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """ Function that compress a folder """
 from datetime import datetime
-from fabric.api import local
-import os
+from fabric.api import *
 
 
 def do_pack():
-    try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        time_zero = datetime.now()
-        fixedDate = "%Y%m%d%H%M%S"
-        archive_path = f'versions/web_static_{time_zero.strftime(fixedDate)}.tgz'
-        local(f'tar -cvzf {archive_path} web_static')
-        return archive_path
-    except:
+    """ making an archive on web_static folder """
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
         return None
